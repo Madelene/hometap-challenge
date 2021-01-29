fa7e36c94c6d1da7f589d981e12546754ca63900
Madelene Campos <madelenecampos@gmail.com>
2021-01-29 14:27:05 -0500

    Adjusts url to make it more user friendly

--- septic_check/urls.py
+++ septic_check/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^properties', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
     url(r'^sewage_info', views.sewage_info, name='sewage-info')
 ]
