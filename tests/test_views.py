999e63638253768bf941b625e69fff7aa2d8ae26
Madelene Campos <madelenecampos@gmail.com>
2021-02-09 14:33:29 -0500

    Adds another test

--- tests/test_views.py
+++ tests/test_views.py
@@ -7,3 +7,10 @@ def test_sewage_info_passing(client):
     uri = reverse('sewage_info')
     resp = client.get(uri)
     assert resp.status_code == 200
+
+
+@pytest.mark.django_db
+def test_home(client):
+    uri = reverse('home')
+    resp = client.get(uri)
+    assert resp.status_code == 200
